829523ca40a4d227c30922e2bfa92093e01a591e
ShuTakenaga <shutakenaga0402@gmail.com>
2021-02-26 00:10:12 +0900

    released

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,10 +6,14 @@ const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
 
 module.exports = {
   siteMetadata: {
-    // You can overwrite values here that are used for the SEO component
-    // Of course you can also add new values here to query them like usual
-    // See all options: https://github.com/LekoArts/gatsby-themes/blob/master/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
+    siteTitle: `Shu_tkng`,
+    siteTitleAlt: `Shu_tkng | portfolio`,
+    siteHeadline: `Shu_tkng | portfolio`,
+    siteUrl: `https://shu-tkng.herokuapp.com/`,
+    siteDescription: `This is Shu's portfolio.`,
+    siteLanguage: `ja`,
+    siteImage: `/banner2.png`,
+    author: `@Shu_tkng`,
   },
   plugins: [
     {
@@ -35,20 +39,21 @@ module.exports = {
         display: `standalone`,
         icons: [
           {
-            src: `/android-chrome-192x192.png`,
+            src: `/icon-192x192.png`,
             sizes: `192x192`,
             type: `image/png`,
           },
           {
-            src: `/android-chrome-512x512.png`,
+            src: `/icon-512x512.png`,
             sizes: `512x512`,
             type: `image/png`,
           },
         ],
-      },
+      }
     },
     `gatsby-plugin-offline`,
     `gatsby-plugin-netlify`,
+    `gatsby-plugin-sass`,
     shouldAnalyseBundle && {
       resolve: `gatsby-plugin-webpack-bundle-analyser-v2`,
       options: {
